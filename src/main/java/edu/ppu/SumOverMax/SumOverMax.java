c331979dc401f318b494b5387206777b0e401e3c
Yaseen.joba <yaseenjoba0@gmail.com>
2020-10-13 09:46:13 +0300

    first SumOverMax

--- src/main/java/edu/ppu/SumOverMax/SumOverMax.java
+++ src/main/java/edu/ppu/SumOverMax/SumOverMax.java
@@ -5,7 +5,10 @@ import java.util.ArrayList;
 public class SumOverMax {
     public int getSumOverMax(ArrayList<Integer>arr)
     {
-        throw new RuntimeException();
+        ListMaxService listMaxService = new ListMaxService();
+        ListSumService listSumService = new ListSumService();
+        int sum = listSumService.getSum(arr);
+        int max = listMaxService.getMax(arr);
+        return sum/max;
     }
-
 }
