1705452ebde3b969c788160564505a9d97687d64
ahtk <ahtk81998@gmail.com>
2020-10-17 07:35:07 +0300

    Fix bugs

--- src/main/java/edu/ppu/SumOverMax/ListMaxService.java
+++ src/main/java/edu/ppu/SumOverMax/ListMaxService.java
@@ -10,8 +10,8 @@ public class ListMaxService {
             throw new NullPointerException();
         int max= Integer.MIN_VALUE;
         for (int i = 0; i<arr.size(); i++){
-            if(arr.indexOf(i)>max)
-                max=arr.indexOf(i);
+            if(arr.get(i)>max)
+                max=arr.get(i);
         }
         return max;
     }
